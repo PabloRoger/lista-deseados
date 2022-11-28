8d735dc6fbec483fccecd5c98f6cac79a6ac4494
PabloRoger <parovi97@gmail.com>
2022-11-28 23:48:09 +0100

    Nueva funcionalidad, colorea los deseos según X segundos

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -29988,15 +29988,48 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 var _classnames = _interopRequireDefault(require("classnames"));
 var _propTypes = _interopRequireDefault(require("prop-types"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 var DeseoItem = function item(_ref) {
   var texto = _ref.texto,
     hecho = _ref.hecho,
     id = _ref.id,
     onItemHecho = _ref.onItemHecho;
+  // Funcionalidad para que cada X segundos cambie de color los items
+  var _useState = (0, _react.useState)(0),
+    _useState2 = _slicedToArray(_useState, 2),
+    edad = _useState2[0],
+    setEdad = _useState2[1];
+  // UseEffect se ejecuta cada vez que se renderiza el componente
+  // Debemos pasarle [props] para que solo se ejecute cuando cambien las props
+  (0, _react.useEffect)(function () {
+    var edadInterval;
+    if (hecho) {
+      setEdad(0);
+    } else {
+      edadInterval = setInterval(function () {
+        if (hecho) {
+          clearInterval(edadInterval);
+        }
+        setEdad(function (a) {
+          return a + 1;
+        });
+      }, 1000);
+    }
+    return function () {
+      return clearInterval(edadInterval);
+    };
+  }, [hecho]);
   return (
     /*#__PURE__*/
     // Con classNames podemos pasarle un objeto con los nombres de las clases
@@ -30005,7 +30038,9 @@ var DeseoItem = function item(_ref) {
     // Recomendable cuando se hace un map
     _react.default.createElement("li", {
       className: (0, _classnames.default)('deseo-lista__item', {
-        'deseo-lista__item--hecho': hecho
+        'deseo-lista__item--hecho': hecho,
+        'deseo-lista__item--warning': edad >= 5 && edad < 10,
+        'deseo-lista__item--danger': edad >= 10
       })
     }, /*#__PURE__*/_react.default.createElement("input", {
       id: id,
@@ -30215,7 +30250,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42723" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35211" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- src/App/DeseoItem/DeseoItem.jsx
+++ src/App/DeseoItem/DeseoItem.jsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 const DeseoItem = function item({
   texto, hecho, id, onItemHecho,
 }) {
+  // Funcionalidad para que cada X segundos cambie de color los items
+  const [edad, setEdad] = useState(0);
+  // UseEffect se ejecuta cada vez que se renderiza el componente
+  // Debemos pasarle [props] para que solo se ejecute cuando cambien las props
+  useEffect(() => {
+    let edadInterval;
+
+    if (hecho) {
+      setEdad(0);
+    } else {
+      edadInterval = setInterval(() => {
+        if (hecho) {
+          clearInterval(edadInterval);
+        }
+        setEdad((a) => a + 1);
+      }, 1000);
+    }
+
+    return () => clearInterval(edadInterval);
+  }, [hecho]);
+
   return (
     // Con classNames podemos pasarle un objeto con los nombres de las clases
     // En este caso, si hecho es true, se agrega la clase deseo-lista__item--hecho
@@ -13,10 +34,17 @@ const DeseoItem = function item({
     <li
       className={classNames('deseo-lista__item', {
         'deseo-lista__item--hecho': hecho,
+        'deseo-lista__item--warning': edad >= 5 && edad < 10,
+        'deseo-lista__item--danger': edad >= 10,
       })}
     >
       {/* Con evento onChange, llama al callback onItemHecho que recibe el estado del checkbox */}
-      <input id={id} checked={hecho} onChange={(e) => onItemHecho(e.target.checked)} type="checkbox" />
+      <input
+        id={id}
+        checked={hecho}
+        onChange={(e) => onItemHecho(e.target.checked)}
+        type="checkbox"
+      />
       {/* htmlFor es para que el label se asocie al input con el id deseado */}
       <label htmlFor={id}>{texto}</label>
     </li>
